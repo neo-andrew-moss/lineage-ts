8cf0bc802fc58ac9e9a8015ed2097267b8867eea
Andrew Moss <andrew@m0ss.dev>
2023-01-14 19:15:32 -0700

    chore: Lint

--- src/find-descendant-imports.ts
+++ src/find-descendant-imports.ts
@@ -1,88 +1,91 @@
 import {
-  Project,
-  type ImportDeclaration,
-  SyntaxKind,
-  SourceFile,
-} from "ts-morph";
-import compact from "lodash/compact";
-import { dbg } from "./util";
+	Project,
+	type ImportDeclaration,
+	SyntaxKind,
+	type SourceFile,
+} from 'ts-morph';
+import compact from 'lodash/compact';
+import {dbg} from './util';
 
 function getImportsFromSourceFileAlt(sourceFile: SourceFile, project: Project) {
-  const imports = new Map<string, Map<string, unknown>>();
-  const queue = [sourceFile];
+	const imports = new Map<string, Map<string, unknown>>();
+	const queue = [sourceFile];
 
-  while (queue.length > 0) {
-    const currentFile = queue.pop();
+	while (queue.length > 0) {
+		const currentFile = queue.pop();
 
-    if (!currentFile) {
-      break;
-    }
+		if (!currentFile) {
+			break;
+		}
 
-    if (!imports.has(currentFile.getFilePath())) {
-      imports.set(currentFile.getFilePath(), new Map<string, unknown>());
-    }
-    currentFile
-      .getDescendantsOfKind(SyntaxKind.ImportDeclaration)
-      .filter((node) => node.isModuleSpecifierRelative())
-      .forEach((node: ImportDeclaration) => {
-        const nodeSourceFile = node.getModuleSpecifierSourceFile();
-        if (!nodeSourceFile) {
-          return;
-        }
-        const importedFilePath = nodeSourceFile.getFilePath();
-        if (!imports.has(importedFilePath)) {
-          imports.set(importedFilePath, new Map<string, unknown>());
-          queue.push(project.getSourceFile(importedFilePath)!);
-        }
-        imports.get(currentFile.getFilePath())!.set(importedFilePath, {});
-      });
-  }
+		if (!imports.has(currentFile.getFilePath())) {
+			imports.set(currentFile.getFilePath(), new Map<string, unknown>());
+		}
 
-  return imports;
+		currentFile
+			.getDescendantsOfKind(SyntaxKind.ImportDeclaration)
+			.filter(node => node.isModuleSpecifierRelative())
+			.forEach((node: ImportDeclaration) => {
+				const nodeSourceFile = node.getModuleSpecifierSourceFile();
+				if (!nodeSourceFile) {
+					return;
+				}
+
+				const importedFilePath = nodeSourceFile.getFilePath();
+				if (!imports.has(importedFilePath)) {
+					imports.set(importedFilePath, new Map<string, unknown>());
+					queue.push(project.getSourceFile(importedFilePath)!);
+				}
+
+				imports.get(currentFile.getFilePath())!.set(importedFilePath, {});
+			});
+	}
+
+	return imports;
 }
 
 type ImportTree = Array<{
-  name: string;
-  children: ImportTree;
+	name: string;
+	children: ImportTree;
 }>;
 
 const getImportsFromSourceFile = (sourceFile: SourceFile): ImportTree => {
-  const imports = sourceFile
-    .getDescendantsOfKind(SyntaxKind.ImportDeclaration)
-    .filter((node) => node.isModuleSpecifierRelative())
-    .map((node: ImportDeclaration) => {
-      const next = node.getModuleSpecifierSourceFile();
+	const imports = sourceFile
+		.getDescendantsOfKind(SyntaxKind.ImportDeclaration)
+		.filter(node => node.isModuleSpecifierRelative())
+		.map((node: ImportDeclaration) => {
+			const next = node.getModuleSpecifierSourceFile();
 
-      if (!next) {
-        console.warn(
-          `Unable to find module specifier for filePath:${sourceFile.getFilePath()}  import: ${node.getModuleSpecifierValue()}`
-        );
-        return undefined;
-      }
+			if (!next) {
+				console.warn(
+					`Unable to find module specifier for filePath:${sourceFile.getFilePath()}  import: ${node.getModuleSpecifierValue()}`,
+				);
+				return undefined;
+			}
 
-      return {
-        name: node.getModuleSpecifierValue(),
-        // This level of recursion is highly problematic
-        children: getImportsFromSourceFile(next),
-      };
-    });
+			return {
+				name: node.getModuleSpecifierValue(),
+				// This level of recursion is highly problematic
+				children: getImportsFromSourceFile(next),
+			};
+		});
 
-  return compact(imports);
+	return compact(imports);
 };
 
 export const findDescendantImports = (filePath: string): ImportTree => {
-  const project = new Project({
-    skipAddingFilesFromTsConfig: true,
-    tsConfigFilePath: "TODO: API this",
-  });
-  project.addSourceFileAtPath(filePath);
-  const sourceFile = project.getSourceFile(filePath);
+	const project = new Project({
+		skipAddingFilesFromTsConfig: true,
+		tsConfigFilePath: 'TODO: API this',
+	});
+	project.addSourceFileAtPath(filePath);
+	const sourceFile = project.getSourceFile(filePath);
 
-  if (!sourceFile) {
-    throw new Error("entrypoint not found");
-  }
+	if (!sourceFile) {
+		throw new Error('entrypoint not found');
+	}
 
-  const f = getImportsFromSourceFileAlt(sourceFile, project);
-  dbg(f);
-  return getImportsFromSourceFile(sourceFile);
+	const f = getImportsFromSourceFileAlt(sourceFile, project);
+	dbg(f);
+	return getImportsFromSourceFile(sourceFile);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
-import chalk from "chalk";
-import clear from "clear";
-import figlet from "figlet";
-import { Command } from "commander";
+import chalk from 'chalk';
+import clear from 'clear';
+import figlet from 'figlet';
+import {Command} from 'commander';
 
-import pkg from "../package.json";
-import { lineage } from "./lineage";
+import pkg from '../package.json';
+import {lineage} from './lineage';
 
 const program = new Command();
 
 clear();
 console.log(
-  chalk.redBright(figlet.textSync(pkg.name, { horizontalLayout: "full" }))
+	chalk.redBright(figlet.textSync(pkg.name, {horizontalLayout: 'full'})),
 );
 
 program
-  .version(pkg.version)
-  .description("descendan-ts")
-  .requiredOption("-f, --file <string>", "entrypoint filepath")
-  .parse(process.argv);
+	.version(pkg.version)
+	.description('descendan-ts')
+	.requiredOption('-f, --file <string>', 'entrypoint filepath')
+	.parse(process.argv);
 
 const options = program.opts();
 
 program.parse();
 
-console.log("opts");
+console.log('opts');
 console.log(options);
 
 lineage(options.file);
 
 if (!process.argv.slice(2).length) {
-  program.outputHelp();
+	program.outputHelp();
 }
